81fc2b6e01b0c5a88d5dc6bdb8f99b8c75f77ffd
HardSoftEngineer <hardsoftengineerperson@gmail.com>
2026-01-04 08:14:54 +0000

    add docker-compose and scripts run-stop

---
+++ scripts/stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+#
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker compose -f "$SCRIPT_DIR/../docker/docker-compose.yml" down 
